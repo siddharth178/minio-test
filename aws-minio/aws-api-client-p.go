7b813ea6f2895933da0c8591211934f16d9f9658
siddharth178 <siddharth.karandikar@gmail.com>
2017-03-02 12:35:39 +0530

    Some code restructuring
    - aws api client to talk to minio server now gets a separate folder of its own.
    - cleaned up some code to handle issues related to use of relative path.

--- aws-minio/aws-api-client-p.go
+++ aws-minio/aws-api-client-p.go
@@ -23,6 +23,7 @@ func upload(fileName string, newSession *session.Session, bucket, key *string, d
 		doneChan <- 1
 	}()
 
+	// open the file and if its not diretory, continue with upload
 	f, err := os.Open(fileName)
 	if err != nil {
 		log.Println(err)
@@ -62,8 +63,7 @@ func uploadWorker(fileNameChan <-chan string, wg *sync.WaitGroup, newSession *se
 		select {
 		case fileName := <-fileNameChan:
 			key := aws.String(fileName)
-			log.Println("Processing:", fileName)
-			//time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+			log.Println("Processing file:", fileName)
 
 			doneChan := make(chan int)
 			go upload(fileName, newSession, bucket, key, doneChan)
@@ -74,6 +74,7 @@ func uploadWorker(fileNameChan <-chan string, wg *sync.WaitGroup, newSession *se
 	}
 }
 
+// Run needed steps to create aws config, get session and create given bucket.
 func doS3Setup(bucketName, accessKey, secretKey, awsServerAddr, region string, useSsl bool) (*session.Session, error) {
 	bucket := aws.String(bucketName)
 
@@ -92,7 +93,7 @@ func doS3Setup(bucketName, accessKey, secretKey, awsServerAddr, region string, u
 		fmt.Println(err)
 		return nil, err
 	}
-	log.Println("Session created using given config")
+	log.Println("AWS session created using given config")
 
 	// create s3 client using session
 	s3Client := s3.New(newSession)
@@ -114,6 +115,7 @@ func doS3Setup(bucketName, accessKey, secretKey, awsServerAddr, region string, u
 	return newSession, nil
 }
 
+// Recursively iterate through all the files in given directory
 func processDirP(dirName string, fileNameChan chan string, wg *sync.WaitGroup) (fileCount int) {
 	log.Println("Processing dir:", dirName)
 	d, err := os.Open(dirName)
@@ -135,6 +137,8 @@ func processDirP(dirName string, fileNameChan chan string, wg *sync.WaitGroup) (
 	return
 }
 
+// If current object is file, send it to uploadWorker through file name chan, for directory, just go inside that
+// directory and look for more files.
 func processFileP(fName string, fileNameChan chan string, wg *sync.WaitGroup) (fileCount int) {
 	f, err := os.Open(fName)
 	if err != nil {
@@ -151,26 +155,49 @@ func processFileP(fName string, fileNameChan chan string, wg *sync.WaitGroup) (f
 		if stat.IsDir() {
 			fileCount += processDirP(fName, fileNameChan, wg)
 		} else {
-			log.Println("Sending to worker:", fName)
-			fileNameChan <- fName
-			wg.Add(1)
-			fileCount++
+			// send absolute path on channel so that s3 doesn't complain about
+			// files with keys containing . or .. or something like that
+			absPath, err := filepath.Abs(fName)
+			if err != nil {
+				log.Println(err)
+			} else {
+				log.Println("Sending file to worker:", absPath)
+				fileNameChan <- absPath
+				wg.Add(1)
+				fileCount++
+			}
 		}
 	}
 	return
 }
 
+func isDir(fileName string) (bool, error) {
+	dir, err := os.Open(fileName)
+	if err != nil {
+		log.Println(err)
+		return false, err
+	}
+	defer dir.Close()
+
+	fInfo, err := dir.Stat()
+	if err != nil {
+		log.Println(err)
+		return false, err
+	}
+	return fInfo.IsDir(), nil
+}
+
 func main() {
 
 	// command line flags
-	accessKey := flag.String("akey", "", "access key id")
-	secretKey := flag.String("skey", "", "secret key")
-	bucketName := flag.String("bucket", "mybucket", "bucket name to be used")
-	awsServerAddr := flag.String("serverAddr", "http://localhost:9000", "aws server to connect to")
-	region := flag.String("region", "us-east-1", "aws region to use")
-	useSsl := flag.Bool("ssl", false, "use SSL while talking to aws")
-	source := flag.String("sourceDir", "", "files from this directory will get uploaded")
-	batchSize := flag.Int("batchSize", 5, "files will be uploaded to s3 in batches of this size")
+	accessKey := flag.String("akey", "", "Access key id")
+	secretKey := flag.String("skey", "", "Secret key")
+	bucketName := flag.String("bucket", "mybucket", "Bucket name to be used")
+	awsServerAddr := flag.String("serverAddr", "http://localhost:9000", "Aws server to connect to")
+	region := flag.String("region", "us-east-1", "Aws region to use")
+	useSsl := flag.Bool("ssl", false, "Use SSL while talking to aws")
+	source := flag.String("sourceDir", "", "Files from this directory will get uploaded")
+	batchSize := flag.Int("batchSize", 5, "Files will be uploaded to s3 in batches of this size")
 	flag.Parse()
 
 	if *accessKey == "" || *secretKey == "" || *source == "" {
@@ -180,22 +207,16 @@ func main() {
 	}
 
 	// before making any AWS related thing, just check if given source exists
-	sourceDir, err := os.Open(*source)
+	ok, err := isDir(*source)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
-	fInfo, err := sourceDir.Stat()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	if !fInfo.IsDir() {
+	if !ok {
 		log.Println("Given source is not a directory")
 		return
 	}
-	sourceDir.Close()
+	log.Println("Uploading files from following dir:", *source)
 
 	// setup s3 session and create bucket
 	newSession, err := doS3Setup(*bucketName, *accessKey, *secretKey, *awsServerAddr, *region, *useSsl)
@@ -204,15 +225,15 @@ func main() {
 		return
 	}
 
-	log.Println("Uploading files from following dir:", fInfo.Name())
-
-	// iterate through list of files in given directory and upload them in parallel
+	// start upload worker and make it listen on file name chan
 	var wg sync.WaitGroup
 	fileNameChan := make(chan string, *batchSize-1)
 
 	// upload worker will wait on a chan for a filename
 	go uploadWorker(fileNameChan, &wg, newSession, aws.String(*bucketName))
 
+	ut1 := time.Now()
+	// iterate through list of files in given directory and upload them in parallel
 	// processDirP will pass in
 	fileCount := processDirP(*source, fileNameChan, &wg)
 
@@ -222,4 +243,5 @@ func main() {
 	log.Println("Waited", time.Now().Sub(t1), "for goroutines to get over")
 
 	log.Println("Upload complete. Files processed:", fileCount)
+	log.Println("Total time required to upload all the files:", time.Now().Sub(ut1))
 }
